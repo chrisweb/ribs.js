7fd78f4d9d2e8eca088f4a11054ddcd33e9fe1c8
Chris Weber <chris963@gmail.com>
2014-08-16 13:56:10 +0200

    router update

--- src/router.js
+++ src/router.js
@@ -11,94 +11,45 @@
  * router
  * 
  * @param {type} Backbone
- * @param {type} Routes
- * @param {type} eventsManager
  * @returns {_L16.Anonym$8}
  */
 define([
-    'backbone',
-    'routes',
-    'ribs.eventsManager'
-], function (Backbone, Routes, eventsManager) {
+    'backbone'
+], function (Backbone) {
     
     'use strict';
     
-    var router;
+    var ribs = {};
     
-    var initialize = function initializeFunction() {
-        
-        var Router = Backbone.Router.extend({
+    ribs.Router = Backbone.Router.extend({
 
-            initialize: function() {
+        initialize: function() {
 
-            },
-            routes: Routes,
-            
-            // this can be removed as soon as the backbone update got released
-            route: function(route, name, callback) {
-                if (!_.isRegExp(route)) route = this._routeToRegExp(route);
-                if (_.isFunction(name)) {
-                    callback = name;
-                    name = '';
-                }
-                if (!callback) callback = this[name];
-                var router = this;
-                Backbone.history.route(route, function(fragment) {
-                    var args = router._extractParameters(route, fragment);
-                    if (router.execute(callback, args, name) !== false) {
-                        router.trigger.apply(router, ['route:' + name].concat(args));
-                        router.trigger('route', name, args);
-                        Backbone.history.trigger('route', router, name, args);
-                    }
-                });
-                return this;
-            },
-            execute: function routerExecute(callback, args, name) {
+        },
+        routes: {},
 
-                // pre-route event
-                eventsManager.trigger('router:preRoute', { 'arguments': args, 'name': name });
-                
-                if (!user.isLogged() && (args.length !== 1 && args[0] !== null)) {
-                    
-                    this.navigate('desktop', { trigger: true });
-                    
-                    return false;
-                    
-                }
-                
-                if (callback) {
-                    
-                    callback.apply(this, args);
-                    
-                }
-
-                // post route event
-                eventsManager.trigger('router:postRoute', { 'arguments': args, 'name': name });
-                
+        // this can be removed as soon as the backbone update got released
+        route: function(route, name, callback) {
+            if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+            if (_.isFunction(name)) {
+                callback = name;
+                name = '';
             }
-
-        });
-        
-        return Router;
-        
-    };
-    
-    var getRrouter = function instantiateFuntion() {
-
-        if (router === undefined) {
-            
-            var Router = initialize();
-            
-            router = new Router();
-            
+            if (!callback) callback = this[name];
+            var router = this;
+            Backbone.history.route(route, function(fragment) {
+                var args = router._extractParameters(route, fragment);
+                if (router.execute(callback, args, name) !== false) {
+                    router.trigger.apply(router, ['route:' + name].concat(args));
+                    router.trigger('route', name, args);
+                    Backbone.history.trigger('route', router, name, args);
+                }
+            });
+            return this;
         }
-        
-        return router;
-        
-    };
 
-    return {
-        start: getRrouter
-    };
+    });
+
+    return ribs.Router;
     
 });
