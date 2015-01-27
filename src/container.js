7721e002f33495fe86d0fb3c400a83a9d6e6ac4d
chrisweb <chris963@gmail.com>
2015-01-27 20:20:15 +0100

    container dispatch options to allow different insert mode

--- src/container.js
+++ src/container.js
@@ -25,6 +25,12 @@ define([
 
     var instance = null;
 
+    /**
+     * 
+     * container constructor
+     * 
+     * @returns {container_L22.ContainerSingleton}
+     */
     var ContainerSingleton = function ContainerSingletonFunction() {
         
         if (instance !== null) {
@@ -39,21 +45,22 @@ define([
     
     ContainerSingleton.prototype = {
         
-        dispatch: function dispatchFunction(containerSelector) {
+        /**
+         * 
+         * dispatch the views of all the container or by a container selector
+         * 
+         * @param {type} containerSelector
+         * @param {type} options
+         * 
+         * @returns {undefined}
+         */
+        dispatch: function dispatchFunction(containerSelector, options) {
             
-            var that = this;
-
             if (containerSelector === undefined) {
 
                 _.each(this.containers, function(views, containerSelector) {
 
-                    _.each(views, function(view) {
-
-                        var viewHtml = view.create();
-
-                        that.bodyElement.find(containerSelector).append(viewHtml);
-
-                    });
+                    dispatchViews.call(this, views, containerSelector, options);
 
                 });
 
@@ -61,18 +68,20 @@ define([
 
                 var views = this.containers[containerSelector];
 
-                _.each(views, function(view) {
-
-                    var viewHtml = view.create();
-
-                    that.bodyElement.find(containerSelector).append(viewHtml);
-
-                });
+                dispatchViews.call(this, views, containerSelector, options);
 
             }
 
         },
 
+        /**
+         * 
+         * add a view to a container
+         * 
+         * @param {type} containerSelector
+         * @param {type} view
+         * @returns {undefined}
+         */
         add: function addFunction(containerSelector, view) {
 
             if (this.containers[containerSelector] === undefined) {
@@ -85,6 +94,16 @@ define([
 
         },
         
+        /**
+         * 
+         * remove a view from the list, for a given selector
+         * just remove the view from the list, don't close it
+         * 
+         * @param {type} containerSelector
+         * @param {type} view
+         * 
+         * @returns {undefined}
+         */
         remove: function removeFunction(containerSelector, view) {
 
             if (this.containers[containerSelector] === undefined) {
@@ -102,7 +121,42 @@ define([
             }
 
         },
+        
+        /**
+         * 
+         * refresh all the container views for a given selector
+         * just rerender the html code and replace the exisitng one with the
+         * newly rendered one, but dont close and then recreate the view
+         * 
+         * @param {type} containerSelector
+         * 
+         * @returns {undefined}
+         */
+        refresh: function emptyFunction(containerSelector) {
+            
+            var views = this.containers[containerSelector];
+            
+            var that = this;
+            
+            _.each(views, function(view) {
+                
+                var viewHtml = view.create();
+                
+                that.bodyElement.find(containerSelector).html(viewHtml);
+                
+            });
+            
+        },
 
+        /**
+         * 
+         * clear the view for a given selector
+         * closes the view and also removes it from the container views list
+         * 
+         * @param {type} containerSelector
+         * 
+         * @returns {undefined}
+         */
         clear: function clearFunction(containerSelector) {
 
             var views = this.containers[containerSelector];
@@ -119,7 +173,47 @@ define([
         
     };
     
-    var getInstance = function getInstanceFunction() {
+    /**
+     * 
+     * (private) dispatch the views
+     * 
+     * @param {type} views
+     * @param {type} containerSelector
+     * @param {type} options
+     * 
+     * @returns {undefined}
+     */
+    var dispatchViews = function(views, containerSelector, options) {
+        
+        var that = this;
+        
+        _.each(views, function(view) {
+
+            var viewHtml = view.create();
+
+            if (options !== undefined
+                && _.has(options, 'insertMode')
+                && options.insertMode === 'prepend') {
+
+                that.bodyElement.find(containerSelector).prepend(viewHtml);
+
+            } else {
+
+                that.bodyElement.find(containerSelector).append(viewHtml);
+
+            }
+
+        });
+        
+    };
+    
+    /**
+     * 
+     * get a container instance
+     * 
+     * @returns {container_L22.ContainerSingleton}
+     */
+    var getInstance = function () {
         
         if (instance === null) {
 
