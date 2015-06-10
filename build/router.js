857ed3456aba14bcbb80df789c68a2cc55a5a0f5
chrisweb <chris963@gmail.com>
2015-06-10 11:11:54 +0200

    new build

---
+++ build/router.js
@@ -0,0 +1,82 @@
+'use strict';
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+(function (deps, factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(deps, factory);
+    }
+})(["require", "exports", 'EventsManager'], function (require, exports) {
+    var EventsManager = require('EventsManager');
+    var Router = (function (_super) {
+        __extends(Router, _super);
+        function Router() {
+            _super.apply(this, arguments);
+        }
+        Router.prototype.initialize = function () {
+        };
+        Router.prototype.route = function (route, name, callback) {
+            var _this = this;
+            if (!_.isRegExp(route))
+                route = this._routeToRegExp(route);
+            if (_.isFunction(name)) {
+                callback = name;
+                name = '';
+            }
+            if (!callback)
+                callback = this[name];
+            Backbone.history.route(route, function (fragment) {
+                var args = _this._extractParameters(route, fragment);
+                // we use a callback function to allow async calls, the
+                // original backbone code uses an if (see below)
+                _this.execute(callback, args, name, function (executeRoute) {
+                    if (executeRoute) {
+                        this.trigger.apply(this, ['route:' + name].concat(args));
+                        this.trigger('route', name, args);
+                        Backbone.history.trigger('route', this, name, args);
+                    }
+                });
+                /*
+                if (router.execute(callback, args, name) !== false) {
+                    
+                    router.trigger.apply(router, ['route:' + name].concat(args));
+                    router.trigger('route', name, args);
+                    Backbone.history.trigger('route', router, name, args);
+                    
+                }
+                */
+            });
+            return this;
+        };
+        Router.prototype.execute = function (callback, routeArguments, routeName, internalCallback) {
+            // pre-route event
+            EventsManager.trigger(EventsManager.constants.ROUTER_PREROUTE, { 'routeArguments': routeArguments, 'routeName': routeName });
+            if (callback)
+                callback.apply(this, routeArguments);
+            // post route event
+            EventsManager.trigger(EventsManager.constants.ROUTER_POSTROUTE, { 'routeArguments': routeArguments, 'routeName': routeName });
+            if (internalCallback !== undefined) {
+                // can return true or false, if false is returned the current
+                // route will get aborted
+                internalCallback(true);
+            }
+            else {
+                // can return true or false, if false is returned the current
+                // route will get aborted
+                return true;
+            }
+        };
+        Router.prototype.getCurrentRoute = function () {
+            return Backbone.history.fragment;
+        };
+        return Router;
+    })(Backbone.Router);
+    exports.Router = Router;
+});
+//# sourceMappingURL=router.js.map
---
+++ build/viewHelper.js
@@ -0,0 +1,30 @@
+'use strict';
+(function (deps, factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(deps, factory);
+    }
+})(["require", "exports"], function (require, exports) {
+    var ViewHelper;
+    (function (ViewHelper) {
+        var viewHelpers = {};
+        function add(helperName, helperCallback) {
+            viewHelpers[helperName] = helperCallback;
+        }
+        ViewHelper.add = add;
+        function remove(helperName) {
+            delete viewHelpers[helperName];
+        }
+        ViewHelper.remove = remove;
+        ;
+        function get() {
+            return viewHelpers;
+        }
+        ViewHelper.get = get;
+        ;
+    })(ViewHelper || (ViewHelper = {}));
+    return ViewHelper;
+});
+//# sourceMappingURL=viewHelper.js.map
