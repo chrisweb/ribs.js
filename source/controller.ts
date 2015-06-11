48cbf0320a947d34ee0dd6d82db83aa51a37d77c
pchab <pierre@chabardes.net>
2015-06-11 17:01:45 +0200

    add create and clear function in controller class

--- source/controller.ts
+++ source/controller.ts
@@ -8,33 +8,38 @@ class Controller {
     options: any;
     router: Backbone.Router;
     onInitialize;
-    
-    constructor () {
-        
-        this.initialize.apply(this, arguments);
+    create;
+
+    constructor(options, configuration, router) {
+
+        this.initialize(options, configuration, router);
         _.extend(this, Backbone.Events);
-        
+
     }
-    
+
     initialize (options, configuration, router) {
-            
+
         this.options = options || {};
         this.router = router;
-            
+
         // if oninitialize exists
         if (this.onInitialize) {
-                
+
             // execute it now
             this.onInitialize(this.options, configuration, this.router);
-                
+
         }
-        
+
+    }
+
+    clear() {
+        (<any>this).off();
     }
 
     static extend() {
         return (<any>Backbone.Model).extend.apply(this, arguments);
     }
-    
+
 }
 
 export = Controller;
