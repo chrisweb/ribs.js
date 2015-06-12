b59a7bd79a12e4d9d8af3670eaa8753871a242ec
pchab <pierre@chabardes.net>
2015-06-12 14:58:21 +0200

    update controller

--- source/controller.ts
+++ source/controller.ts
@@ -1,39 +1,52 @@
 'use strict';
 
 import Backbone = require('backbone');
+import _ = require('underscore');
 
-class Controller extends Backbone.Events {
+class Controller {
 
     options: any;
     router: Backbone.Router;
     onInitialize;
-    
-    constructor () {
-        
-        super();
-        this.initialize.apply(this, arguments);
-        
+    create;
+    _promise;
+
+    get promise() {
+        return this._promise;
     }
-    
-    initialize (options, configuration, router) {
-            
+
+    set promise(value) {
+        this._promise = value;
+    }
+
+    clear(): void { }
+
+    constructor(options, configuration, router) {
+
+        this.initialize(options, configuration, router);
+        _.extend(this, Backbone.Events);
+
+    }
+
+    initialize(options, configuration, router) {
+
         this.options = options || {};
         this.router = router;
-            
+
         // if oninitialize exists
         if (this.onInitialize) {
-                
+
             // execute it now
             this.onInitialize(this.options, configuration, this.router);
-                
+
         }
-        
+
     }
 
     static extend() {
         return (<any>Backbone.Model).extend.apply(this, arguments);
     }
-    
+
 }
 
 export = Controller;
